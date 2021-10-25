4a846fabd801b2a5572d06c3f30f3a4c28c032fe
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2021-10-25 08:12:01 +0700

    flash configuration

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import express from "express";
 import router from "./routes/router.js";
 import db from "./config/db.js";
 import expressLayouts from "express-ejs-layouts";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import flash from "connect-flash";
 
 // app initialization
 const app = express();
+
+// configure dotenv
+dotenv.config({ path: "./config/config.env" });
 
 // initializing port
 const PORT = process.env.port || 5000;
@@ -21,6 +28,15 @@ try {
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(expressLayouts);
+app.use(
+	session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+app.use(cookieParser(process.env.SECRET));
+app.use(flash());
 
 // connecting to routes
 app.use(router);
